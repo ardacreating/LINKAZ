f8b2e857ef84f7abafe8777f5ece81beac7337cd
Miraç <88139688+westydev@users.noreply.github.com>
2023-01-12 13:37:57 +0400

    Update Config.js

--- src/Settings/Config.js
+++ src/Settings/Config.js
@@ -1,6 +1,6 @@
 module.exports = {
     WEBSITE: {
         port: 80,
-        connection: "mongodb+srv://westydev:westydev1337@westydevbots.j8ngkko.mongodb.net/LINKAZ?retryWrites=true&w=majority"
+        connection: ""
     }
 }
